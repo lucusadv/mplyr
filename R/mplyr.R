8d36f1a0c9254bf9414b8222c88444ae8e7462aa
contravariant <paleologo@gmail.com>
2015-06-19 11:34:08 -0400

    minor changes to vignette

--- R/mplyr.R
+++ R/mplyr.R
@@ -1,6 +1,6 @@
 #' mplyr: Library for matrix manipulation 
 #'    
-#' @import reshape2 ggplot2 gridExtra dplyr magrittr zoo
+#' @import reshape2 ggplot2 gridExtra dplyr magrittr zoo Gmisc
 #' @docType package
 #' @name mplyr
 #' @author G.A.Paleologo 
