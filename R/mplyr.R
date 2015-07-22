26c9b9b75069e040f5696951cc94c147f9588a1b
contravariant <paleologo@gmail.com>
2015-07-22 13:18:19 -0400

    removed external dependency on Gmisc

--- R/mplyr.R
+++ R/mplyr.R
@@ -1,6 +1,6 @@
 #' mplyr: Library for matrix manipulation 
 #'    
-#' @import lazyeval gridExtra ggplot2 reshape2 dplyr magrittr ensurer testthat Gmisc
+#' @import lazyeval gridExtra ggplot2 reshape2 dplyr magrittr ensurer testthat
 #' @docType package
 #' @name mplyr
 #' @author G.A.Paleologo 
